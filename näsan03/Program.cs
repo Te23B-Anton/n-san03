9119b23b14a3455082dd2eb3b6d423c808d63580
Anton Faren <Anton.faren@elev.ga.ntig.se>
2024-11-19 13:21:06 +0100

    first commit

---
+++ näsan03/Program.cs
@@ -0,0 +1,25 @@
+﻿static void Hello32()
+{
+    for (int i = 0; i < 32; i++)
+    {
+        Console.WriteLine($"hello , world {i + 1}");
+    }
+}
+
+static float Kvadrat(int num){
+    return MathF.Pow(num, 2);
+}
+
+static void sigma(int num){
+   Console.WriteLine(MathF.Pow(num , 2));
+
+}
+
+static float multi(int num){
+    return 2*num;
+}
+
+
+System.Console.WriteLine(multi((int)Kvadrat(4)));
+
+Console.ReadLine();
