cf4794246317a99870b8dd5027155867c12c2628
Anton Faren <Anton.faren@elev.ga.ntig.se>
2024-11-26 13:04:51 +0100

    näsan3

--- näsan03/Program.cs
+++ näsan03/Program.cs
@@ -1,4 +1,16 @@
-﻿static void Hello32()
+﻿using System.Globalization;
+
+
+string[] namn = ["Sigma boy", "fortnite", "Wow man", "Anton"];
+
+System.Console.WriteLine(GetChoice(namn));
+
+
+
+
+
+
+static void Hello32()
 {
     for (int i = 0; i < 32; i++)
     {
@@ -6,20 +18,36 @@
     }
 }
 
-static float Kvadrat(int num){
+static float Kvadrat(int num)
+{
     return MathF.Pow(num, 2);
 }
 
-static void sigma(int num){
-   Console.WriteLine(MathF.Pow(num , 2));
+static void sigma(int num)
+{
+    Console.WriteLine(MathF.Pow(num, 2));
 
 }
 
-static float multi(int num){
-    return 2*num;
+static float multi(int num)
+{
+    return 2 * num;
 }
 
 
-System.Console.WriteLine(multi((int)Kvadrat(4)));
+string GetChoice(string[] nice)
+{
+    for (int i = 0; i < nice.Length; i++)
+    {
+        System.Console.WriteLine($"{i + 1}. {nice[i]}");
+    
+    }
+    int num ;
+    while (!int.TryParse(Console.ReadLine(), out num) || num <  1 || num > nice.Length)
+    {
+        System.Console.WriteLine("Skriv ett giltigt nummer!");
+    }
+    return nice[num - 1];
+}
 
 Console.ReadLine();
--- näsan03/obj/Debug/net8.0/näsan03.AssemblyInfo.cs
+++ näsan03/obj/Debug/net8.0/näsan03.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("näsan03")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9119b23b14a3455082dd2eb3b6d423c808d63580")]
 [assembly: System.Reflection.AssemblyProductAttribute("näsan03")]
 [assembly: System.Reflection.AssemblyTitleAttribute("näsan03")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
